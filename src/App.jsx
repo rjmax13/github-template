937374fe463ccc381a9d81ad9fdcd5ce2dd09b52
Rod <rod_max@sbcglobal.net>
2025-05-13 15:13:18 -0500

    adding to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,16 @@ function App() {
         <div className="main_event">
           <div className="first_page_name_btn">
             <h1 className="budget_heading">Conference Expense Planner</h1>
-            <p className="budget_sentence"> Plan your next major event with us!</p>
+            <p className="budget_sentence">
+              {" "}
+              Plan your next major event with us!
+            </p>
             <div className="getstarted_btn">
-              <button onClick={() => handleGetStarted()} className="get-started-btn">
-                Get Started
+              <button
+                onClick={() => handleGetStarted()}
+                className="get-started-btn"
+              >
+                Get Started Planning
               </button>
             </div>
           </div>
@@ -29,7 +35,7 @@ function App() {
         </div>
       </header>
 
-      <div className={`event-list-container ${showVenue ? 'visible' : ''}`}>
+      <div className={`event-list-container ${showVenue ? "visible" : ""}`}>
         <ConferenceEvent />
       </div>
     </>
