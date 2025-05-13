aae96c699769b21f320d07838c598870d716dbf6
Rod <rod_max@sbcglobal.net>
2025-05-13 15:02:57 -0500

    changed project name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "github-template", //base name for gh-pages
+  base: "ibm-react-final-project", //base name for gh-pages
 });
